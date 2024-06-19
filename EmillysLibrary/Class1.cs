f1c858c83116c2abf9dfa0e6f42afba8c86cd078
Emily Morgan <emorgan921@gmail.com>
2024-06-19 10:21:06 -0400

    Add project files.

---
+++ EmillysLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace EmillysLibrary;
+
+public class Class1
+{
+    public static string About {get;set;} = "MyClass1";
+
+    public string About2 {get;set;} = "MyClass1 and About2";
+}
---
+++ LibrariesExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using EmillysLibrary;
+namespace LibrariesExample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Class1.About");
+
+        //Console.WriteLine(Class1.About2"); cannont do this bc About2 is not static, meaning you can't just create an instance of the class. You need to define the property before you can reference the class within this class. 
+
+
+    }
+
+
+        var c1 = new Class1();
+        Console.WriteLine(c1About2);
+}
